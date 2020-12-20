c2b72f221877ac50c04e8587c8fc0dc661a2f3da
quarxdmz <quarxdmz@gmail.com>
2020-12-21 06:46:25 +0800

    first commit

---
+++ src/components/Owners.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="owners">
+    <div>
+      <label for="name">Name</label>
+      <input type="text" v-model="name" />
+    </div>
+    <div>
+      <label for="phone_number">Phone Number</label>
+      <input type="text" v-model="phone_number" />
+    </div>
+    <div>
+      <label for="email_address">Email Address</label>
+      <input type="text" v-model="email_address" />
+    </div>
+    <div>
+      <label for="address">Address</label>
+      <input type="text" v-model="address" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Owners',
+  data() {
+    return {
+      name: '',
+      phone_number: '',
+      email_address: '',
+      address: '',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
